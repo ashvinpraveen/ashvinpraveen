b9cd7c46c33bc669407e42b41af92c4f6c549709
Ashvin Praveen <ashvinpraveen@gmail.com>
2025-09-14 22:00:02 +0800

    Updated TipTap and got it working!

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Ashvin Praveen';
-export const SITE_DESCRIPTION = 'Personal website of Ashvin Praveen';
+export const SITE_TITLE = 'Peepol';
+export const SITE_DESCRIPTION = 'Peepol - The best place to find people';
